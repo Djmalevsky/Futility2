49d7e97a2891226c6f40fb4505045b5287f8aa0e
Djmalevsky <56096106+Djmalevsky@users.noreply.github.com>
2020-12-20 13:38:52 -0500

    Add files via upload

---
+++ Models/machinegun.gd
@@ -0,0 +1,17 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var ammo = 100
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+#func _input(event):
+	#if Input.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
